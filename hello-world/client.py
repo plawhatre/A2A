b412a154e5ab5d8933f1ca08ba3663f8bda6d3b5
prashant <prashantlawhatre@gmail.com>
2025-06-09 20:30:50 +0530

    added rich print

--- hello-world/client.py
+++ hello-world/client.py
@@ -9,6 +9,7 @@ from a2a.types import (
     SendMessageRequest,
     SendStreamingMessageRequest
 )
+from rich import print
 
 PUBLIC_AGENT_CARD_PATH = '/.well-known/agent.json'
 EXTENDED_AGENT_CARD_PATH = 'agent/authenticatedExtendedCard'
