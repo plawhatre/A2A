6d7b277885c35ee4715da85d8dcae20bec663eb0
prashant <prashantlawhatre@gmail.com>
2025-06-10 17:46:48 +0530

    added agent class

--- langgraph/agent.py
+++ langgraph/agent.py
@@ -1,8 +1,10 @@
 import httpx
+from collections.abc import AsyncIterable
 from pydantic import BaseModel
 from typing import Literal
 
 from langchain_core.tools import tool
+from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langgraph.prebuilt import create_react_agent
@@ -80,7 +82,63 @@ class CurrencyAgent:
             response_format=ResponseFormat
         )
 
+    def invoke(self, query, context_id) -> str:
+        inputs = {'messages': [HumanMessage(query)]}
+        config = {'configurable': {'thread_id': context_id}}
+        self.graph.invoke(inputs, config=config)
+        return self.get_agent_response(config)
+
+    async def stream(self, query, context_id) -> AsyncIterable:
+        inputs = {'messages': [HumanMessage(query)]}
+        config = {'configurable': {'thread_id': context_id}}
+        for item in self.graph.stream(inputs, config=config, stream_mode='values'):
+            message = item['messages'][-1]
+            if (
+                isinstance(message, AIMessage)
+                and message.tool_calls
+                and len(message.tool_calls) > 0
+            ): 
+                yield {
+                    'is_task_complete': False,
+                    'require_user_input': False,
+                    'content': 'Looking up the exchange rates...'
+                }
+            elif isinstance(message, ToolMessage):
+                yield {
+                    'is_task_complete': False,
+                    'require_user_input': False,
+                    'content': 'Processing the exchange rates...'
+                }
+            yield self.get_agent_response(config)
 
 
-if __name__ == "__main__":
-    pass
+    def get_agent_response(self, config):
+        current_state = self.graph.get_current_state(config)
+        structured_response = current_state.values.get('structured_response')
+        if (
+            structured_response
+            and isinstance(structured_response, ResponseFormat)
+        ):
+            if (
+                structured_response.status == 'input_required'
+                or structured_response.status == 'error'
+            ):
+                return {
+                    'is_task_complete': False,
+                    'require_user_input': True,
+                    'content': structured_response.message
+                }
+            if structured_response.status == 'complete':
+                return {
+                    'is_task_complete': True,
+                    'require_user_input': False,
+                    'content': structured_response.message
+                }
+        return {
+            'is_task_complete': False,
+            'require_user_input': True,
+            'content': (
+                "We are unable to process your request. "
+                "Please try after some time."
+            )
+        }
