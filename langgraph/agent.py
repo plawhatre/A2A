454229862effc85c8a6302955b533a713c4e5597
prashant <prashantlawhatre@gmail.com>
2025-06-10 17:03:29 +0530

    added constructor for agent class

--- langgraph/agent.py
+++ langgraph/agent.py
@@ -5,7 +5,20 @@ from typing import Literal
 from langchain_core.tools import tool
 from langchain_google_genai import ChatGoogleGenerativeAI
 
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+
 API_ADDRESS = 'https://api.frankfurter.app/'
+SYSTEM_INSTRUCTIONS = (
+    "You are a specialised agent for currency conversion. "
+    "Your sole purpose is to use the 'get_exchange_rate' tool to answer the questions. "
+    "If the user asks anything that is not about currency conversion or exchange rate "
+    "then, politely state that you can not help with the topic and can only assist in currency related queries. "
+    "Set the response status to input required if the user need to provide more information. "
+    "Set the response status to error if there is an error while processing the request. "
+    "Set the response status to complete if the request is complete."
+)
+MEMORY = MemorySaver()
 
 @tool
 def get_exchange_rate(
@@ -52,6 +65,21 @@ class ResponseFormat(BaseModel):
     """
     status: Literal['input_required', 'complete', 'error'] = 'input_required'
     message: str
+
+class CurrencyAgent:
+    """Its a specialised agent for currency conversion
+    """
+    def __init__(self):
+        self.model = ChatGoogleGenerativeAI(model='gemini-2.0-flash')
+        self.tools = [get_exchange_rate]
+        self.graph = create_react_agent(
+            model=self.model,
+            tools=self.tools,
+            prompt=SYSTEM_INSTRUCTIONS,
+            checkpointer=MEMORY,
+            response_format=ResponseFormat
+        )
+
 
 
 if __name__ == "__main__":
